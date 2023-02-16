9a0983332a4200c4f221a23b70ea4fed405fbafc
Paula blesa <paulablesa6@gmail.com>
2023-02-15 22:51:04 -0300

    concluido

---
+++ AULA-1_Atividade/app.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const calcular = document.getElementById('calcular')
+
+function somar() {
+
+    const nota1 = Number(document.getElementById('nota1').value)
+    const nota2 = Number(document.getElementById('nota2').value)
+    const resultado = document.getElementById('resultado')
+    const formulario = document.getElementById('form')
+   
+    const media = (nota1 + nota2) / 2
+
+
+    console.log(media)
+    if (media >= 5) {
+        formulario.classList.remove('notaVermelha')
+        formulario.classList.add('notaAzul')
+        resultado.value = 'Aprovado'
+
+    } else {
+        resultado.value = 'Reprovado'
+        formulario.classList.add('notaVermelha')
+    }
+}
+
+calcular.addEventListener('click', somar)
